8b701b0c51905e792568c249d34bc276dc1c0f1f
Igor Bozhennikov <mr.igorr@gmail.com>
2019-02-03 22:27:30 +0300

    view object

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='blog_posts_list'),
-    path('post/<str:slug>/', post_detail, name='blog_post_detail'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='blog_post_detail'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='blog_tag_detail'),
+    path('tag/', tag_list, name='blog_tag_list'),
 ]
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
-from .models import Post
+from django.views.generic import View
+from .models import *
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    return render(request, 'blog/post_list.html', context={'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tag_list.html', context={'tags': tags})
+
+
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
