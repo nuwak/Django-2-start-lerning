8dce3c4eaf8d17b88be574e6585c6cb68f1dfa7d
Igor Bozhennikov <mr.igorr@gmail.com>
2019-02-07 00:30:02 +0300

    stuff

--- app/blogengine/blog/utils.py
+++ app/blogengine/blog/utils.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db import models
 
 
 class ObjectDetailMixin:
@@ -9,3 +10,11 @@ class ObjectDetailMixin:
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+
+class ObjectListMixin:
+    model: models.Model = None
+
+    def tag_list(self, request):
+        tags = self.model.objects.all()
+        return render(request, 'blog/tag_list.html', context={'tags': tags})
