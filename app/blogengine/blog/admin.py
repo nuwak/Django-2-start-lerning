585f732cf658d3fe8fc35571ecf08c801a8d2e90
Igor Bozhennikov <mr.igorr@gmail.com>
2019-02-08 19:26:55 +0300

    add link to admin tutorial

--- app/blogengine/blog/admin.py
+++ app/blogengine/blog/admin.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+# see https://djbook.ru/rel1.9/intro/tutorial07.html#adding-related-objects
 admin.site.register(Post)
 
