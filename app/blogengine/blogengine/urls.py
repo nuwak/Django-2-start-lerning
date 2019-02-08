d456bae0bdeb0f5cc841ba65c19dc6315b67645b
Igor Bozhennikov <mr.igorr@gmail.com>
2019-02-08 19:11:03 +0300

    debug toolbar

--- app/blogengine/blogengine/urls.py
+++ app/blogengine/blogengine/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path
 from .views import *
@@ -20,6 +21,7 @@ from django.urls import include
 
 urlpatterns = [
     path('', home, name='home'),
+    path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls, name='admin_panel'),
     path('blog/', include('blog.urls')),
 ]
